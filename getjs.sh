9d067234ee4dadf6df873d652d5a898ab31937e2
Jamie Steiner <jvsteiner@gmail.com>
2015-01-22 15:12:07 +0200

    updated jquery, bootstrap, and added logo

--- getjs.sh
+++ getjs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget -P static/ https://code.jquery.com/jquery-1.8.2.min.js
+wget -P static/ https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min.js
 wget -P static/ https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js
 wget -P static/ https://ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1.js
 wget -P static/ https://cdnjs.cloudflare.com/ajax/libs/knockout.mapping/2.4.1/knockout.mapping.min.js
